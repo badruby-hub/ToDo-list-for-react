9938fbc7c7f26d5874dd56009cd1fc5bc5558292
bad-ruby hub <nazim.fataliev@list.ru>
2024-11-26 21:36:04 +0400

    add navigation for header

---
+++ components/pages/Header.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import classes from "@/components/pages/Header.module.css";
+
+const 
+   pages = [
+    {href:'/', title: 'home'},
+    {href:'/ToDo', title:'ToDo-list'}
+   ];
+
+export function Header(){
+    return <header>
+<nav className={classes.navigation}>
+    <ul>
+    {pages.map(({href,title})=> <li key={href}>
+        <Link href={href}>{title}</Link>
+    </li>)}
+    </ul>
+</nav>
+    </header>
+}
---
+++ pages/ToDo.tsx
@@ -0,0 +1,7 @@
+import { ToDoList } from "@/components/ToDo";
+
+export default function ToDO(){
+return<>
+ <ToDoList/>
+</>
+}
