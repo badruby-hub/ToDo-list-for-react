17999ef859cee82f6ed676077ada89da01030121
bad-ruby hub <nazim.fataliev@list.ru>
2024-12-09 08:56:55 +0400

    adding neon

---
+++ pages/api/todo/[[...path]].js
@@ -0,0 +1,15 @@
+import { neon } from "@neondatabase/serverless";
+
+    const sql = neon(process.env.DATABASE_URL);
+    
+
+export default async function todo(req,res){
+ const 
+    { path} = req.query,
+    id = path?.[0];
+    console.log("***", { id});
+    const rows = await sql`SELECT * FROM todo`; 
+    res.status(200).json(
+    rows   
+    )
+}
